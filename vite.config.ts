418d1c5a452d5df2a89934cdb71ed35e4e017246
Sean Kruzel <skruzel@astrocyte.io>
2024-03-13 10:17:37 -0400

    added sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { vitePlugin as remix } from '@remix-run/dev'
 import { flatRoutes } from 'remix-flat-routes'
 import { defineConfig } from 'vite'
@@ -6,31 +7,35 @@ const MODE = process.env.NODE_ENV
 
 export default defineConfig({
 	build: {
-		cssMinify: MODE === 'production',
-		rollupOptions: {
+        cssMinify: MODE === 'production',
+
+        rollupOptions: {
 			external: [/node:.*/, 'stream', 'crypto', 'fsevents'],
 		},
-	},
-	plugins: [
-		remix({
-			ignoredRouteFiles: ['**/*'],
-			serverModuleFormat: 'esm',
-			routes: async defineRoutes => {
-				return flatRoutes('routes', defineRoutes, {
-					ignoredRouteFiles: [
-						'.*',
-						'**/*.css',
-						'**/*.test.{js,jsx,ts,tsx}',
-						'**/__*.*',
-						// This is for server-side utilities you want to colocate next to
-						// your routes without making an additional directory.
-						// If you need a route that includes "server" or "client" in the
-						// filename, use the escape brackets like: my-route.[server].tsx
-						'**/*.server.*',
-						'**/*.client.*',
-					],
-				})
-			},
-		}),
-	],
+
+        sourcemap: true
+    },
+	plugins: [remix({
+        ignoredRouteFiles: ['**/*'],
+        serverModuleFormat: 'esm',
+        routes: async defineRoutes => {
+            return flatRoutes('routes', defineRoutes, {
+                ignoredRouteFiles: [
+                    '.*',
+                    '**/*.css',
+                    '**/*.test.{js,jsx,ts,tsx}',
+                    '**/__*.*',
+                    // This is for server-side utilities you want to colocate next to
+                    // your routes without making an additional directory.
+                    // If you need a route that includes "server" or "client" in the
+                    // filename, use the escape brackets like: my-route.[server].tsx
+                    '**/*.server.*',
+                    '**/*.client.*',
+                ],
+            })
+        },
+    }), sentryVitePlugin({
+        org: "closedloop",
+        project: "autoetl-www"
+    })],
 })
